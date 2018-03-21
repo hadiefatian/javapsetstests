40dd7f5f4d84761b4d2ce68ba734289e6e95c35c
Hadi <hadi.effatian@gmail.com>
2018-03-21 19:00:03 +0100

    Created Spring Template

---
+++ src/main/java/at/refugeescode/javapsetstests/JavapsetstestsApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.javapsetstests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapsetstestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavapsetstestsApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/javapsetstests/JavapsetstestsApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.javapsetstests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavapsetstestsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
